3f28e8d48658de9bec6f0d3abe12b21d160b589c
DongYeon, Yu <48278678+ksr20612@users.noreply.github.com>
2023-07-06 16:45:29 +0900

    Time: 82 ms (50.24%), Space: 45.9 MB (19.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,7 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-    for(let i=0; i<nums.length; i++) {
-        for(let j=i+1; j<nums.length; j++) {
-            if(nums[i] + nums[j] === target) return [i, j];
+    const valueMap = new Map();
+    for(let i=0; i < nums.length; i++) {
+        valueMap.set(nums[i], i);
+    }
+    for(let i=0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if(valueMap.has(complement) && valueMap.get(complement) !== i) {
+            return [i, valueMap.get(complement)]
         }
     }
 };
