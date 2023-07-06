5e21f3d661c2a9968d4f0cad20d7f4726b27252a
DongYeon, Yu <48278678+ksr20612@users.noreply.github.com>
2023-07-06 19:46:24 +0900

    Time: 146 ms (99.15%), Space: 52.4 MB (21.73%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,10 +1,12 @@
 function isPalindrome(x: number): boolean {
     if(x < 0) return false;
     if(x < 10) return true;
-    const xStringified = x.toString();
     
-    for(let i=0; i<Math.floor(xStringified.length/2); i++) {
-        if(xStringified[i] !== xStringified[xStringified.length - 1 - i]) return false;
+    let reverse = 0;
+    let temp = x;
+    while(temp != 0) {
+        reverse = reverse * 10 + temp % 10;
+        temp = Math.floor(temp / 10);
     }
-    return true;
+    return reverse === x;
 };
