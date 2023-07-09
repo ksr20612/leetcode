c8d65024b90642f5c47da6de7230711a3e93d476
DongYeon, Yu <48278678+ksr20612@users.noreply.github.com>
2023-07-09 20:54:10 +0900

    Time: 70 ms (12.84%), Space: 42.4 MB (86.87%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
@@ -0,0 +1,9 @@
+const dp = [];
+
+function tribonacci(n: number): number {
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+    if(n === 2) return 1;
+    if(dp[n]) return dp[n];
+    return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+};
